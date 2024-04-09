6bcb3d18a0f25a901a70d1c6cd5932a72e1e3834
akipopyross <118900195+akipopyross@users.noreply.github.com>
2024-04-09 23:59:04 +0300

    upload to github: Hello World!

---
+++ bot_utilities/ai_utils.py
@@ -0,0 +1,108 @@
+import aiohttp
+import io
+import time
+import random
+import nest_asyncio
+from langdetect import detect
+from gtts import gTTS
+from urllib.parse import quote
+from bot_utilities.config_loader import load_current_language, config
+from g4f.client import Client
+from g4f.Provider import RetryProvider, FlowGpt, ChatgptAi, Liaobots
+from dotenv import load_dotenv
+
+load_dotenv()
+nest_asyncio.apply()
+
+current_language = load_current_language()
+internet_access = config['INTERNET_ACCESS']
+
+client = Client(
+    provider=RetryProvider([FlowGpt, ChatgptAi, Liaobots]),
+    # image_provider=BingCreateImages,
+    # No image provider works lmao
+)
+
+async def generate_response(instructions, history):
+    messages = [
+            {"role": "system", "name": "instructions", "content": instructions},
+            *history,
+        ]
+    response = client.chat.completions.create(
+        model=config['GPT_MODEL'],
+        messages=messages
+    )
+    message = response.choices[0].message.content
+    return message
+
+async def generate_gpt4_response(prompt):
+    messages = [
+            {"role": "system", "name": "admin_user", "content": prompt},
+        ]
+    response = client.chat.completions.create(
+        model='gpt-4',
+        messages=messages
+    )
+    message = response.choices[0].message.content
+    return message
+
+async def poly_image_gen(session, prompt):
+    seed = random.randint(1, 100000)
+    image_url = f"https://image.pollinations.ai/prompt/{prompt}?seed={seed}"
+    async with session.get(image_url) as response:
+        image_data = await response.read()
+        return io.BytesIO(image_data)
+
+async def generate_image_prodia(prompt, model, sampler, seed, neg):
+    print("\033[1;32m(Prodia) Creating image for :\033[0m", prompt)
+
+    start_time = time.time()
+    async def create_job(prompt, model, sampler, seed, neg):
+        if neg is None:
+            negative = "(nsfw:1.5),verybadimagenegative_v1.3, ng_deepnegative_v1_75t, (ugly face:0.8),cross-eyed,sketches, (worst quality:2), (low quality:2), (normal quality:2), lowres, normal quality, ((monochrome)), ((grayscale)), skin spots, acnes, skin blemishes, bad anatomy, DeepNegative, facing away, tilted head, {Multiple people}, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worstquality, low quality, normal quality, jpegartifacts, signature, watermark, username, blurry, bad feet, cropped, poorly drawn hands, poorly drawn face, mutation, deformed, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, extra fingers, fewer digits, extra limbs, extra arms,extra legs, malformed limbs, fused fingers, too many fingers, long neck, cross-eyed,mutated hands, polar lowres, bad body, bad proportions, gross proportions, text, error, missing fingers, missing arms, missing legs, extra digit, extra arms, extra leg, extra foot, repeating hair, nsfw, [[[[[bad-artist-anime, sketch by bad-artist]]]]], [[[mutation, lowres, bad hands, [text, signature, watermark, username], blurry, monochrome, grayscale, realistic, simple background, limited palette]]], close-up, (swimsuit, cleavage, armpits, ass, navel, cleavage cutout), (forehead jewel:1.2), (forehead mark:1.5), (bad and mutated hands:1.3), (worst quality:2.0), (low quality:2.0), (blurry:2.0), multiple limbs, bad anatomy, (interlocked fingers:1.2),(interlocked leg:1.2), Ugly Fingers, (extra digit and hands and fingers and legs and arms:1.4), crown braid, (deformed fingers:1.2), (long fingers:1.2)"
+        else:
+            negative = neg
+        url = 'https://api.prodia.com/generate'
+        params = {
+            'new': 'true',
+            'prompt': f'{quote(prompt)}',
+            'model': model,
+            'negative_prompt': f"{negative}",
+            'steps': '100',
+            'cfg': '9.5',
+            'seed': f'{seed}',
+            'sampler': sampler,
+            'upscale': 'True',
+            'aspect_ratio': 'square'
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params) as response:
+                data = await response.json()
+                return data['job']
+            
+    job_id = await create_job(prompt, model, sampler, seed, neg)
+    url = f'https://api.prodia.com/job/{job_id}'
+    headers = {
+        'authority': 'api.prodia.com',
+        'accept': '*/*',
+    }
+
+    async with aiohttp.ClientSession() as session:
+        while True:
+            async with session.get(url, headers=headers) as response:
+                json = await response.json()
+                if json['status'] == 'succeeded':
+                    async with session.get(f'https://images.prodia.xyz/{job_id}.png?download=1', headers=headers) as response:
+                        content = await response.content.read()
+                        img_file_obj = io.BytesIO(content)
+                        duration = time.time() - start_time
+                        print(f"\033[1;34m(Prodia) Finished image creation\n\033[0mJob id : {job_id}  Prompt : ", prompt, "in", duration, "seconds.")
+                        return img_file_obj
+
+async def text_to_speech(text):
+    bytes_obj = io.BytesIO()
+    detected_language = detect(text)
+    tts = gTTS(text=text, lang=detected_language)
+    tts.write_to_fp(bytes_obj)
+    bytes_obj.seek(0)
+    return bytes_obj
